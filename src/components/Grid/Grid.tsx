f6a480d376ebf63d5583e672cfad79155c9608b2
Ricardo Henrique Vinciguerra <ricardorhv.dev@gmail.com>
2022-07-11 20:53:58 -0300

    Now is ok

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Button from '../Button'
 import { Container } from './styles'
 import Header from '../Header'
 import Sidebar from '../Sidebar'
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,3 @@ export {default as Sidebar} from './Sidebar';
 export {default as Timetable} from './Timetable';
 export {default as Header} from './Header';
 export {default as Grid} from './Grid';
-export {default as Button} from './Button';
